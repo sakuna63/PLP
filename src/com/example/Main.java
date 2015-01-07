d21e2c8c0c1f6cfbf56d9770a28a8b8b9664493d
Ippei Nawate <saku.ni63@gmail.com>
2015-01-07 16:16:52 +0900

    課題1-2

--- src/com/example/Main.java
+++ src/com/example/Main.java
@@ -2,13 +2,14 @@ package com.example;
 
 import com.example.Token.Operation;
 
+import javax.naming.OperationNotSupportedException;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, OperationNotSupportedException {
 //        if (args.length < 1) {
 //            throw new IllegalArgumentException("Usage: java -jar plp.jar formula-file");
 //        }
@@ -23,7 +24,7 @@ public class Main {
         }
     }
 
-    private static int interpret(String line) {
+    private static int interpret(String line) throws OperationNotSupportedException {
         AST ast = parse(line);
         return eval(ast);
     }
@@ -49,53 +50,88 @@ public class Main {
         }
     }
 
-    private static AST parse(String line) {
+    private static AST parse(String line) throws OperationNotSupportedException {
         StringTokenIterator iterator = new StringTokenIterator(line);
-        Token token;
         AST preAST = null;
-        while (!(token = iterator.next()).equals(Token.EOL)) {
-            switch (token.operation) {
-                case NUM:
-                    preAST = new AST(token);
-                    break;
-                case PLUS:
-                    preAST = readExpr(preAST, iterator);
-                    break;
-                case MINUS:
-                    preAST = readExpr(preAST, iterator);
-                    break;
-                case MULTIPLY:
-                    preAST = readTerm(preAST, iterator);
-                    break;
-                case DIVIDE:
-                    preAST = readTerm(preAST, iterator);
-                    break;
-                default:
-                    throw new IllegalStateException("");
-            }
+        while (!iterator.next().equals(Token.EOL)) {
+            preAST = read(preAST, iterator);
         }
         return preAST;
     }
 
-    private static AST readTerm(AST preAST, StringTokenIterator iterator) {
+    private static AST read(AST preAST, StringTokenIterator iterator) throws OperationNotSupportedException {
+        Token token = iterator.current();
+        switch (token.operation) {
+            case NUM:
+            case PAR_L:
+                preAST = readNum(iterator);
+                break;
+            case PLUS:
+                preAST = readExpr(preAST, iterator);
+                break;
+            case MINUS:
+                preAST = readExpr(preAST, iterator);
+                break;
+            case MULTIPLY:
+                preAST = readTerm(preAST, iterator);
+                break;
+            case DIVIDE:
+                preAST = readTerm(preAST, iterator);
+                break;
+            default:
+                throw new IllegalStateException(String.valueOf(token.operation));
+        }
+        return preAST;
+    }
+
+    private static AST readNum(StringTokenIterator iterator) throws OperationNotSupportedException {
+        Token token = iterator.current();
+        switch (token.operation) {
+            case NUM:
+                return new AST(token);
+            case PAR_L:
+                return readParenthesis(iterator);
+            default:
+                throw new OperationNotSupportedException();
+        }
+    }
+
+    private static AST readParenthesis(StringTokenIterator iterator) throws OperationNotSupportedException {
+        Token token;
+        AST preAST = null;
+        while (!(token = iterator.next()).equals(Token.EOL)) {
+            switch (token.operation) {
+                case PAR_R:
+                    return preAST;
+                default:
+                    preAST = read(preAST, iterator);
+            }
+        }
+        throw new IllegalArgumentException("parenthesis is not closed with ')'");
+    }
+
+    private static AST readTerm(AST preAST, StringTokenIterator iterator) throws OperationNotSupportedException {
         AST ast = new AST(iterator.current());
         if (preAST.operation == Operation.PLUS || preAST.operation == Operation.MINUS) {
             ast.left = preAST.right;
             preAST.right = ast;
-            ast.right = new AST(iterator.next());
+            iterator.next();
+            ast.right = readNum(iterator);
             return preAST;
         }
         else {
             ast.left = preAST;
-            ast.right = new AST(iterator.next());
+            iterator.next();
+            ast.right = readNum(iterator);
             return ast;
         }
     }
 
-    private static AST readExpr(AST preAST, StringTokenIterator iterator) {
+    private static AST readExpr(AST preAST, StringTokenIterator iterator) throws OperationNotSupportedException {
         AST ast = new AST(iterator.current());
         ast.left = preAST;
-        ast.right = new AST(iterator.next());
+        iterator.next();
+        ast.right = readNum(iterator);
         return ast;
     }
 
